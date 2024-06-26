9847756094dd87a65e5d81af0eb4155e5290316d
holysun1 <113403659+holysun1@users.noreply.github.com>
2024-06-26 17:15:22 -0300

    Operadores.java

---
+++ tipois-variaveis/src/Operadores.java
@@ -0,0 +1,27 @@
+package Operadores;
+public class Operadores {
+    public static void main(String[] args) {
+        int numero = 5;
+        System.out.println(numero);
+        numero = numero +2;
+        //Sera incrementado +2 no numero final
+        System.out.println(numero);
+        numero = ++ numero;
+        //Implementado +1 no numero final;
+        System.out.println(numero);
+        numero = -- numero;
+        //Decrementado -1 no numero final;
+        System.out.println(numero);
+
+        boolean variavel = true;
+        System.out.println(variavel);
+        //Valor original da variável
+        System.out.println(!variavel);
+        //Alteração do valor da variável
+        System.out.println(variavel);
+        //Valor original da variavel
+        variavel = !variavel;
+        //Alteração do valor da variavél
+        System.out.println(variavel);
+
+    }
